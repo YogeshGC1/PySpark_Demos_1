d8cd2f5bfa7e9d0bcc3b19d64040ec13365b8e7a
Yogesh Chavan <sychavan8899@gmail.com>
2022-11-03 17:41:14 +0530

    Working Souce Code For pyspark Demo for wordcount

---
+++ Pyspark_first_demo.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+
+if __name__ == '__main__':
+    print("Demo PySpark Application Started")
+
+    spark=SparkSession \
+          .builder \
+          .appName("WordCount") \
+          .master("local[*]") \
+          .getOrCreate()
+
+    sc=spark.sparkContext
+
+    input_file_path="C:/Users/NEW/Desktop/project/source/WordCount.txt"
+    text_file=sc.textFile(input_file_path)
+    Counts=text_file.flatMap(lambda line: line.split(" ")) \
+                    .map(lambda word: (word, 1)) \
+                    .reduceByKey(lambda x, y:x+y)
+
+    output=Counts.collect()
+    for (word,Counts) in output:
+        print("%s:%i"%(word,Counts))
