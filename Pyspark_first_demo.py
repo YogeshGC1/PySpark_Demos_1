d6899689b3b7afd1b732870415e2e07cd4f7b53b
Yogesh Chavan <sychavan8899@gmail.com>
2022-11-03 18:14:39 +0530

    New commit - Working Souce Code For pyspark Demo for wordcount

--- Pyspark_first_demo.py
+++ Pyspark_first_demo.py
@@ -20,3 +20,6 @@ if __name__ == '__main__':
     output=Counts.collect()
     for (word,Counts) in output:
         print("%s:%i"%(word,Counts))
+
+
+# Working Source code of word count program in pyspark
